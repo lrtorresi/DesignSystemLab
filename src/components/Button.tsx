4a5094820abef246eb995b9b8c1eb8815344e95f
Rodrigo Torresi <rodrigo.torresi@impar.com.br>
2022-10-13 20:15:33 -0300

    init commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+
+
+
+
+export interface ButtonProps {
+    children: ReactNode;
+    asChildren?: boolean;
+}
+
+
+
+export function Button({ children, asChildren }: ButtonProps) {
+
+    const Comp = asChildren ? Slot : 'button';
+
+    return (
+        <Comp className={clsx(
+            'py-4 px-3 bg-cyan-500 rounded font-semibold text-back text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
+        )}>
+
+        {children}
+
+        </Comp>
+    )
+}
